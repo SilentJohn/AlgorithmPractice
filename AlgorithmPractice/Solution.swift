2504c20543595c426eeaabd636aae19e8896f222
john.jiang <john.jiang@51job.com>
2019-06-13 19:05:45 +0800

    LeetCode 459. 重复的字符串
    未完成

--- AlgorithmPractice/Solution.swift
+++ AlgorithmPractice/Solution.swift
@@ -313,4 +313,39 @@ class Solution {
         }
         return result
     }
+    
+    /**
+     LeetCode 459. Repeated Substring Pattern
+     Given a non-empty string check if it can be constructed by taking a substring of it and appending multiple copies of the substring together. You may assume the given string consists of lowercase English letters only and its length will not exceed 10000.
+     
+       
+     
+     Example 1:
+     
+     Input: "abab"
+     Output: True
+     Explanation: It's the substring "ab" twice.
+     Example 2:
+     
+     Input: "aba"
+     Output: False
+     Example 3:
+     
+     Input: "abcabcabcabc"
+     Output: True
+     Explanation: It's the substring "abc" four times. (And the substring "abcabc" twice.)
+     */
+    func repeatedSubstringPattern(_ s: String) -> Bool {
+        guard let fisrt = s.first else {
+            return false
+        }
+        for (i, c) in s[s.index(after: s.startIndex) ..< s.endIndex].enumerated() {
+            var sub: String? = nil
+            if c == fisrt {
+                
+            }
+        }
+        
+        return false
+    }
 }
--- AlgorithmTests/AlgorithmTests.swift
+++ AlgorithmTests/AlgorithmTests.swift
@@ -91,4 +91,10 @@ class AlgorithmTests: XCTestCase {
     func testTotalHammingDistance() {
         XCTAssert(solution.totalHammingDistance([4, 14, 2]) == 6)
     }
+    
+    func testRepeatedSubstringPattern() {
+        XCTAssert(solution.repeatedSubstringPattern("abab"))
+        XCTAssertFalse(solution.repeatedSubstringPattern("aba"))
+        XCTAssert(solution.repeatedSubstringPattern("abcabcabcabc"))
+    }
 }
